ac5eeae627f43b354eb4362af68dfc51ff18f523
HerrBergenholtz <zbdv8800@gmail.com>
2023-11-24 11:21:21 +0100

    Commit

---
+++ Uppgift 1 - REDUPLIKATION/Uppgift 1 - REDUPLIKATION/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Reduplikation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ordet?");
+            string ord = Console.ReadLine();
+
+            Console.WriteLine("Antal upprepningar?");
+            int upprepningar = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < upprepningar; i++)
+            {
+                Console.Write(ord);
+            }
+        }
+    }
+}
---
+++ Uppgift 2 - Tre i Rad/Uppgift 2 - Tre i Rad/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Tre_i_Rad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int treigaTal = 0;
+            Console.WriteLine("N?");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i < n; i++)
+            {
+                if (i * (i + 1) * (i + 2) < n) 
+                {
+                    treigaTal++;
+                }
+            }
+            Console.WriteLine(treigaTal);
+        }
+    }
+}
